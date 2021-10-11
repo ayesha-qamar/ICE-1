6a81b3b29a9f1d0eb578718f5156dcc495cdd444
ayesha qamar <qamaraye@sheridancollege.ca>
2021-10-11 04:42:17 -0400

    Added comments

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
@@ -7,7 +7,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author AYESHA QAMAR 991490866
  */
 public class Card {
 
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,7 +3,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author AYESHA QAMAR 991490866
  */
 public class CardTrick {
     public static void main(String[] args)
